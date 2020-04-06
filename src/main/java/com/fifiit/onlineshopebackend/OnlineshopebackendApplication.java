536847ee24958116bf84d3dd45b0b6d7536e6805
Milkis  Ghebremairma <milkiasts@gmail.com>
2020-04-05 22:55:50 -0700

    intial

---
+++ src/main/java/com/fifiit/onlineshopebackend/OnlineshopebackendApplication.java
@@ -0,0 +1,13 @@
+package com.fifiit.onlineshopebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineshopebackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineshopebackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fifiit/onlineshopebackend/OnlineshopebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fifiit.onlineshopebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineshopebackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
